5ac0af7e6fb8841a6e60ac3cb962130f7e6ff11c
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-03-22 01:46:48 -0700

    Updated dependencies in order to use the latest firebase api's. Added Firebase FireStoreUI dependency. Included an example google-services.json file as an helper to guide cloners/forkers on where to place it.

--- app/src/main/java/me/ayahya/aesirr/twisentials/MainActivity.java
+++ app/src/main/java/me/ayahya/aesirr/twisentials/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MobileAds.initialize(this, getString(R.string.mobile_ads_app_id));
+        MobileAds.initialize(this, getString(R.string.mobile_ads_APP_ID));
     }
 }
