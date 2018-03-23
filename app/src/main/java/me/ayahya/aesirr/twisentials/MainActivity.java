4444f2735cd0d62a3d57a882853fa67b0a171729
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-03-22 21:07:21 -0700

    Added Firebase crashlytics. Included ex. usage

--- app/src/main/java/me/ayahya/aesirr/twisentials/MainActivity.java
+++ app/src/main/java/me/ayahya/aesirr/twisentials/MainActivity.java
@@ -2,15 +2,22 @@ package me.ayahya.aesirr.twisentials;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.crash.FirebaseCrash;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MobileAds.initialize(this, getString(R.string.mobile_ads_APP_ID));
+
+//        ## Firebase Crashalytics usage example ##
+//        FirebaseCrash.log("Activity created");
+//        FirebaseCrash.logcat(Log.ERROR, TAG, "NPE caught");
+//        FirebaseCrash.report(ex);
     }
 }
